5619a6fbcc141fcc8eaffcd8106a7a8db38e04d8
Yuva Sri Sai <nanithota18102004@gmail.com>
2025-09-05 15:28:45 +0530

    wallet connection done in driver , business and admin dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ErrorBoundary from './ErrorBoundary'
-import { AuthProvider } from './hooks/useAuth.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ErrorBoundary from "./ErrorBoundary";
+import { AuthProvider } from "./hooks/useAuth.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThirdwebProvider } from "thirdweb/react";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
       <AuthProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <ThirdwebProvider>
+            <App />
+          </ThirdwebProvider>
+        </QueryClientProvider>
       </AuthProvider>
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
